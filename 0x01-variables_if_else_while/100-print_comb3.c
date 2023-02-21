5c71a50ef410c8dcc44044fa3ffeacaa3def1706
ndukasamuel <chuksyyoung929@gmail.com>
2023-02-20 20:55:53 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: Always success
+ */
+
+int main(void)
+{
+	int d, p;
+
+	for (d = '0'; d < '9'; d++)
+	{
+	for (p = d + 1; p <= '9'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(',');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
